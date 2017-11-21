0606338dc2fd74bc75229a54d4f8ce0bf680c7e1
NatalyaLemeshevskaya <33570956+NatalyaLemeshevskaya@users.noreply.github.com>
2017-11-21 20:30:27 +0300

    HomeWork3.Task1

--- HomeWork/HomeWork3/src/by/itacademy/Task1/Main.java
+++ HomeWork/HomeWork3/src/by/itacademy/Task1/Main.java
@@ -6,36 +6,87 @@ import java.util.Scanner;
 public class Main {
    public static Scanner in= new Scanner(System.in);
     public static void main(String []arg){
-        int k=array(10);
+        int[] array=new int[10];
 
+        vvod(array);
+        Sorted(array,array,array);
+        evenNumbers(array);
 
-    }
+         }
 
-    public static int array(int num){
-        int [] array= new int[num];
+    public static void vvod(int[] Ar){
         int i=0;
-        for (int element : array)
+        for (int element : Ar)
         {
 
             System.out.print("Введите " + (i + 1) + "-й элемент массива: ");
-            array[i] = in.nextInt();
-            System.out.println(array[i] + " - ");
+            Ar[i] = in.nextInt();
+            System.out.println(Ar[i] + " - ");
             i++;
         }
-        for(i=0;i<array.length;i++){
-            System.out.println(Arrays.toString(array));
-            break;}
 
+        Vivod(Ar);
 
+     }
 
-         return array;
-
-     //   for (int element : array)
-
-            //System.out.print(element + " ");
-
-
+   public static void Vivod (int[]Arra ) {
+       System.out.println(Arrays.toString(Arra));
 
    }
+
+   public static void Sorted(int[] Ar,int[] Ar2,int[]Ar3){
+        // сортировка пузырьком
+       for (int i=Ar.length-1; i>=0; i--)
+       {
+           for (int j=0; j<i; j++)
+           {
+               if (Ar[j] > Ar[j+1])
+               {
+                   int tmp = Ar[j];
+                   Ar[j] = Ar[j+1];
+                   Ar[j+1] = tmp;
+               }
+           }
+       }
+
+       //сортировка выбором
+       for (int i = 0; i < Ar2.length - 1; i++) {
+              int min_i = i;
+           for (int j = i + 1; j < Ar.length; j++) {
+               if (Ar2[j] < Ar2[min_i]) {
+                   min_i = j;
+               }
+           }
+           int temp = Ar2[i];
+           Ar2[i] = Ar2[min_i];
+           Ar2[min_i] = temp;
+       }
+
+
+       //сортировка встравками
+       int i, j, value;
+
+       for(i = 1; i <Ar3.length; i++) {
+           value = Ar3[i];
+           for (j = i - 1; j >= 0 && Ar3[j] > value; j--) {
+               Ar3[j + 1] = Ar3[j];
+           }
+           Ar3[j + 1] = value;
+       }
+
+       Vivod(Ar);
+       Vivod(Ar2);
+       Vivod(Ar3);
+   }
+   public static void evenNumbers(int[]Arr){
+       for(int i=0;i< Arr.length;i++){
+           if( Arr[i]%2==0 && Arr[i]!=0){
+                System.out.print(Arr[i]+"|");
+           }
+
+       }
+
+   }
+
 
 }
