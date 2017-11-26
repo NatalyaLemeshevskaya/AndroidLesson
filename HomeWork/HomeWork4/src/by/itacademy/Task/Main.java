915056085d855f00c627c93acde14a53448e0e7b
NatalyaLemeshevskaya <33570956+NatalyaLemeshevskaya@users.noreply.github.com>
2017-11-26 17:11:18 +0300

    change homework4

--- HomeWork/HomeWork4/src/by/itacademy/Task/Main.java
+++ HomeWork/HomeWork4/src/by/itacademy/Task/Main.java
@@ -1,23 +1,97 @@
 package by.itacademy.Task;
 
+import java.awt.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[]arg){
 
-        Pacient[] pacient = new Pacient[5];
-        pacient[0] = new  Pacient ("Петров",25,"диабет");
-        pacient[0] = new  Pacient ("Иванов",46,"язва");
-        pacient[0] = new  Pacient ("Трубкин",13,"ангина");
-        pacient[0] = new  Pacient ("Серов",18,"бронхит");
-        pacient[0] = new  Pacient ("Левин",85,"инсульт");
+        Pacient[] pacient = new Pacient[2];
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Нажмите 1 для поиска по имени, 2 - по возрасту,3 - по болезни");
+        for(int i=0;i<pacient.length;i++) {
+            pacient[i] = new Pacient();
 
 
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите имя пациента № "+(i+1)+":");
+            pacient[i].setName(in.next());
+            System.out.println("Введите диагноз пациента № "+(i+1)+":");
+            pacient[i].setDisease(in.next());
+            System.out.println("Введите возраст пациента № "+(i+1)+":");
+            pacient[i].setAge(in.nextInt());
+
+
+        }
+        for( Pacient i: pacient){
+            printPeople(i);
+        }
 
 
 
-     }
+      Scanner in = new Scanner(System.in);
+      System.out.println("Нажмите 1 для поиска по имени, 2 - по возрасту,3 - по болезни");
+      int a = in.nextInt();
+
+      switch (a){
+
+          case 1: {
+
+              System.out.println("Введите имя пациента: ");
+              String name=in.next();
+
+              for(Pacient pacient1:pacient){
+                  if(pacient1.getName().contains(name)){
+                      printPeople(pacient1);
+                    }
+                    else{
+                      System.out.println("Данного пациента нет в нашей базе ");
+                      break;
+                  }
+              break;
+              }
+
+          }
+
+          case 2:{
+              System.out.println("Введите возраст пациента: ");
+              int age=in.nextInt();
+              for(Pacient pacient1:pacient){
+                  if(pacient1.getAge() == age){
+                      printPeople(pacient1);
+                  }
+                  else{
+                      System.out.println("Данного пациента нет в нашей базе ");
+                  }
+                  break;
+              }
+              break;
+
+          }
+          case 3:{
+              System.out.println("Введите диагноз пациента: ");
+              String disease=in.next();
+              for(Pacient pacient1:pacient){
+                  if(pacient1.getDisease().contains(disease)){
+                      printPeople(pacient1);
+                  }
+                  else{
+                      System.out.println("Пациента с таким диагнозом нет в нашей базе ");
+                  }
+                  break;
+              }
+               break;
+          }
+          default:
+              System.out.println("Вы ввели неправильную цифру");
+
+      }
+
+
+    }
+    public static void printPeople(Pacient pacient){
+
+        System.out.println("Пациент "+pacient.getName()+" в возрасте "+pacient.getAge()+", диагноз - "+pacient.getDisease() );
+
+    }
 }
--- HomeWork/HomeWork4/src/by/itacademy/Task/Pacient.java
+++ HomeWork/HomeWork4/src/by/itacademy/Task/Pacient.java
@@ -5,11 +5,38 @@ public class Pacient {
     private String name;
     private int age;
     private String disease;
+    private boolean present= false;
 
-    public Pacient(String name, int age, String disease) {
-        this.name = name;
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
         this.disease = disease;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPresent() {
+        return present;
+    }
+
+    public void setPresent(boolean present) {
+        this.present = present;
     }
 }
 
